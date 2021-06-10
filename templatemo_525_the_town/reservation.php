5a76f0bd936a23e7b99f891943ac68cba7fcf0f2
kimshin <hanmo2000110@gmail.com>
2021-06-10 17:11:11 +0900

    debug

--- templatemo_525_the_town/reservation.php
+++ templatemo_525_the_town/reservation.php
@@ -123,7 +123,8 @@
                             echo "</br>";
                             echo "</br>";
                             echo "</br>";
-                            echo "<p>이름: <input type="text" name="name_of_borrower" size=40><br></br>학번: <input type="text" name="stdid" size=40><br><p>";
+                            echo "<p>이름: <input type=\"text\" name=\"name_of_borrower\" size=40><br></br>학번: <input type=\"text\" name=\"stdid\" size=40><br><p>";
+                            
                             echo "</br>";
                             echo "</br>";
                             echo "</br>";
@@ -133,9 +134,9 @@
                             echo "</br>";
                             echo "</br>";
                             echo "</br>";
-                            echo "<a href="index.php" class="tm-intro-text tm-btn-primary">return</a>";
+                            echo "<a href=\"index.php\" class=\"tm-intro-text tm-btn-primary\">return</a>";
                             echo "&nbsp;";
-                            echo "<button type="button" onclick="test()" >submit</button>";
+                            echo "<button type=\"button\" onclick=\"test()\" >submit</button>";
                             echo "</br>";
                             echo "</br>";
                             echo "</br>";
