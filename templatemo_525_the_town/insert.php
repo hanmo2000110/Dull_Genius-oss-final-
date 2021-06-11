7cbaf7b806755d4c0ae19828caae828953719adb
kimshin <hanmo2000110@gmail.com>
2021-06-11 18:39:29 +0900

    finally

--- templatemo_525_the_town/insert.php
+++ templatemo_525_the_town/insert.php
@@ -1,27 +1,31 @@
 <?php
-$servername = "localhost";
-$username = "admin";
-$password = "Name0413";
-$dbname = "Dull_Genius";
- 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
- 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
- 
-// prepare and bind
-$stmt = $conn->prepare("INSERT INTO reservation (borrower, studentId, boardgameborrowed, reservationAt) VALUES (?, ?, ?, NOW() )");
-$stmt->bind_param("sss", $borrower, $studentId, $boardgameborrowed);
- 
-// set parameters and execute
-$borrower = $_GET['name_of_borrower'];
-$studentId = $_GET['stdid'];
-$boardgameborrowed = $_GET['stdid'];
-$stmt->execute();
-echo $_GET['borrower'];
+    $servername = "localhost";
+    $username = "admin";
+    $password = "Name0413";
+    $dbname = "Dull_Genius";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    // prepare and bind
+    $stmt = $conn->prepare("INSERT INTO reservation (borrower, studentId, boardgameborrowed, reservationAt) VALUES (?, ?, ?, NOW() )");
+    $stmt->bind_param("sss", $borrower, $studentId, $boardgameborrowed);
+    
+    // set parameters and execute
+    $borrower = $_GET['name_of_borrower'];
+    $studentId = $_GET['stdid'];
+    $name = $_GET['checked'];
+
+    foreach ($name as $board){ 
+        $boardgameborrowed = $board;
+        $stmt->execute();
+    }
+
 
 ?>
 <!DOCTYPE html>
