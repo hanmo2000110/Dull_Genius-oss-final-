3341d7f897e6b7d19116d86faf3c73a789005b77
kimshin <hanmo2000110@gmail.com>
2021-06-11 21:32:24 +0900

    dsfsd

--- templatemo_525_the_town/insert.php
+++ templatemo_525_the_town/insert.php
@@ -32,7 +32,7 @@
         foreach ($name as $board){ 
             $boardgameborrowed = $board;
             $stmt->execute();
-            mysqli_query($conn, "UPDATE reservation SET status = reserved WHERE title = ".$boardgameborrowed);
+            mysqli_query($conn, "UPDATE reservation SET status = 'reserved' WHERE title = ".$boardgameborrowed."\"");
 
         }
     }
